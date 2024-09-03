e14d47dbf75cbfa4b505e870b1e19d89d47f2e15
Braylin <braylinvasquez20@gmail.com>
2024-09-02 20:43:30 -0400

    Agregano detalles de diseño

--- app/src/main/java/edu/ucne/prioridades/MainActivity.kt
+++ app/src/main/java/edu/ucne/prioridades/MainActivity.kt
@@ -1,12 +1,15 @@
 package edu.ucne.prioridades
 
+import android.graphics.Paint.Style
 import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +18,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
@@ -26,6 +30,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,6 +39,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
@@ -43,6 +50,8 @@ import edu.ucne.prioridades.database.PrioridadDb
 import edu.ucne.prioridades.entities.PrioridadEntity
 import edu.ucne.prioridades.ui.theme.PrioridadesTheme
 import kotlinx.coroutines.launch
+import androidx.lifecycle.compose.LocalLifecycleOwner
+
 
 class MainActivity : ComponentActivity() {
     private lateinit var prioridadDb: PrioridadDb
@@ -57,11 +66,9 @@ class MainActivity : ComponentActivity() {
         ).fallbackToDestructiveMigration()
             .build()
 
-
         setContent {
             PrioridadesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
@@ -79,9 +86,11 @@ class MainActivity : ComponentActivity() {
     ) {
         var descripcion by remember { mutableStateOf("") }
         var diasComrpomiso by remember { mutableStateOf("") }
-        var errorMessage: String? by remember { mutableStateOf(null) }
+        var errorMessageDes: String? by remember { mutableStateOf(null) }
+        var errorMessageDias: String? by remember { mutableStateOf("") }
 
         Scaffold {innerPadding ->
+
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -91,30 +100,45 @@ class MainActivity : ComponentActivity() {
                 ElevatedCard(
                     modifier = Modifier.fillMaxWidth()
                 ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(15.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ){
+                        Text(text = "Registro de Prioridades", style= MaterialTheme.typography.headlineMedium)
+                    }
                     Column(
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(8.dp)
                     ) {
                         OutlinedTextField(
-                            label = {Text(text = "Descripcion")},
+                            label = {Text(text = "Descripción")},
                             value = descripcion,
                             onValueChange =  {descripcion = it},
-                            modifier = Modifier.fillMaxWidth()
+                            modifier = Modifier.fillMaxWidth(),
                         )
+                        Text(text = errorMessageDes.orEmpty(), color = MaterialTheme.colorScheme.error)
                         OutlinedTextField(
-                            label = {Text(text="Dias Compromiso")},
+                            label = {Text(text="Días Compromiso")},
                             value = diasComrpomiso,
                             onValueChange = {diasComrpomiso = it},
-                            modifier = Modifier.fillMaxWidth()
+                            modifier = Modifier.fillMaxWidth(),
+                            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
                         )
+                        Text(text = errorMessageDias.orEmpty(), color = MaterialTheme.colorScheme.error)
                         Spacer(modifier = Modifier.padding(2.dp))
                         Row(
-                            modifier = Modifier.fillMaxWidth()
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
                         ){
                             OutlinedButton(
                                 onClick = {
-
+                                    descripcion = ""
+                                    diasComrpomiso = ""
+                                    errorMessageDes = null
+                                    errorMessageDias = null
                                 }
                             ) {
                                 Icon(
@@ -126,15 +150,29 @@ class MainActivity : ComponentActivity() {
                             val scope = rememberCoroutineScope()
                             OutlinedButton(
                                 onClick = {
-                                    scope.launch {
-                                        savePrioridad(
-                                            PrioridadEntity(
-                                                descripcion = descripcion,
-                                                diasCompromiso = diasComrpomiso.toIntOrNull()
+                                    val newDiasCompromiso = diasComrpomiso.toIntOrNull()
+                                    if(descripcion.isBlank()){
+                                        errorMessageDes = "Este campo es requerido"
+                                    }
+                                    if(newDiasCompromiso == null){
+                                        errorMessageDias = "Este campo es requerido"
+                                    }
+                                    if(newDiasCompromiso!! > 31 || newDiasCompromiso < 1){
+                                        errorMessageDias = "Este campo debe estar entre 1 y 31"
+                                    }
+                                    else{
+                                        scope.launch {
+                                            savePrioridad(
+                                                PrioridadEntity(
+                                                    descripcion = descripcion,
+                                                    diasCompromiso = newDiasCompromiso
+                                                )
                                             )
-                                        )
-                                        descripcion= ""
-                                        diasComrpomiso = ""
+                                            descripcion= ""
+                                            diasComrpomiso = ""
+                                            errorMessageDes = null
+                                            errorMessageDias = null
+                                        }
                                     }
                                 }
                             ) {
@@ -147,7 +185,7 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 }
-                val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
+                val lifecycleOwner = LocalLifecycleOwner.current
                 val prioridadList by prioridadDb.prioridadDao().getAll()
                     .collectAsStateWithLifecycle(
                         initialValue = emptyList(),
@@ -167,10 +205,23 @@ class MainActivity : ComponentActivity() {
             modifier = Modifier.fillMaxSize()
         ) {
             Spacer(modifier = Modifier.height(32.dp))
-            Text("Lista de Prioridades")
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+            ){
+                Text(
+                    text= "Lista de Prioridades",
+                    style = MaterialTheme.typography.titleLarge,
+
+
+                )
+            }
 
             LazyColumn(
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier.fillMaxSize(),
             ) {
                 items(prioridadList){
                     PrioridadRow(it)
@@ -183,8 +234,15 @@ class MainActivity : ComponentActivity() {
     @Composable
     private fun PrioridadRow(it: PrioridadEntity){
         Row(
-            verticalAlignment = Alignment.CenterVertically
-
+            modifier = Modifier.padding(5.dp)
+        ){
+            Text("Id", modifier = Modifier.weight(1f))
+            Text("Descripción", modifier = Modifier.weight(2f))
+            Text("Días Compromiso", modifier = Modifier.weight(2f))
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(5.dp)
         ){
             Text(
                 modifier = Modifier.weight(1f),
@@ -193,7 +251,6 @@ class MainActivity : ComponentActivity() {
             Text(
                 modifier = Modifier.weight(2f),
                 text = it.descripcion,
-                style = MaterialTheme.typography.headlineLarge
             )
             Text(
                 modifier = Modifier.weight(2f),
@@ -205,13 +262,19 @@ class MainActivity : ComponentActivity() {
 
     private suspend fun savePrioridad(prioridad: PrioridadEntity){
         prioridadDb.prioridadDao().save(prioridad)
-
     }
+
+
     @Preview(showBackground = true, showSystemUi = true)
     @Composable
     fun PrioridadScreenPreview() {
+        var prioridadList: List<PrioridadEntity> = listOf(
+            PrioridadEntity(1, "Alta", 10),
+            PrioridadEntity(2, "Media", 5),
+            PrioridadEntity(3, "Baja", 1)
+        )
         PrioridadesTheme {
-            PrioridadScreen()
+            PrioridadListScreen(prioridadList)
         }
     }
 }
