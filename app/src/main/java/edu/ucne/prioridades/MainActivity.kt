f2db3d60ae356d3dbce642cce5bbd27d243469b9
Braylin <braylinvasquez20@gmail.com>
2024-09-06 23:31:56 -0400

    Optimizando Imports

--- app/src/main/java/edu/ucne/prioridades/MainActivity.kt
+++ app/src/main/java/edu/ucne/prioridades/MainActivity.kt
@@ -5,23 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 import androidx.room.Room
 import edu.ucne.prioridades.Data.dao.database.PrioridadDb
 import edu.ucne.prioridades.Data.dao.entities.PrioridadEntity
 import edu.ucne.prioridades.presentation.navigation.PrioridadNavHost
-import edu.ucne.prioridades.presentation.navigation.Screen
-import edu.ucne.prioridades.presentation.navigation.prioridad.PrioridadListScreen
-import edu.ucne.prioridades.presentation.navigation.prioridad.PrioridadScreen
 import edu.ucne.prioridades.ui.theme.PrioridadesTheme
 
 
