6585c9fd9596aed9c16ec4a5d1e7de4304d9cf11
Braylin <braylinvasquez20@gmail.com>
2024-09-06 21:00:56 -0400

    Agregando los archivos Dao,Db y Entity a una carpeta Data

--- app/src/main/java/edu/ucne/prioridades/Data/dao/database/PrioridadDb.kt
+++ app/src/main/java/edu/ucne/prioridades/Data/dao/database/PrioridadDb.kt
@@ -1,9 +1,9 @@
-package edu.ucne.prioridades.database
+package edu.ucne.prioridades.Data.dao.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import edu.ucne.prioridades.dao.PrioridadDao
-import edu.ucne.prioridades.entities.PrioridadEntity
+import edu.ucne.prioridades.Data.dao.dao.PrioridadDao
+import edu.ucne.prioridades.Data.dao.entities.PrioridadEntity
 
 @Database(
     entities = [
