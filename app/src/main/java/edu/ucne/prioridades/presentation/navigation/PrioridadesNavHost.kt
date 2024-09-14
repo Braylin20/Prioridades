94e3a3fa3a2dc90d7db2eb74499eba92a7fc3cbd
Braylin <braylinvasquez20@gmail.com>
2024-09-14 14:36:41 -0400

    Agregando AppBar al NavHost

--- app/src/main/java/edu/ucne/prioridades/presentation/navigation/PrioridadesNavHost.kt
+++ app/src/main/java/edu/ucne/prioridades/presentation/navigation/PrioridadesNavHost.kt
@@ -1,38 +1,84 @@
 package edu.ucne.prioridades.presentation.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import edu.ucne.prioridades.Data.dao.database.PrioridadDb
+import edu.ucne.prioridades.presentation.navigation.components.ModalDrawerSheet
 import edu.ucne.prioridades.presentation.navigation.prioridad.PrioridadListScreen
 import edu.ucne.prioridades.presentation.navigation.prioridad.PrioridadScreen
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PrioridadNavHost(
     navHostController: NavHostController,
 ) {
-    NavHost(
-        navController = navHostController,
-        startDestination = Screen.PrioridadList
+    val coroutineScope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = true,
+        drawerContent = {
+            ModalDrawerSheet(coroutineScope, drawerState, navHostController)
+        },
     ) {
-        composable<Screen.PrioridadList> {
-            PrioridadListScreen(
-                createPrioridad = {navHostController.navigate(Screen.Prioridad(0))},
-                goToPrioridadScreen = {navHostController.navigate(Screen.Prioridad(it))}
-            )
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("") },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            coroutineScope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(Icons.Rounded.Menu, contentDescription = "Menu Botton")
+                        }
+                    }
+                )
+            }
+        ) { innerpadding ->
+            NavHost(
+                navController = navHostController,
+                startDestination = Screen.Home,
+                modifier = Modifier.padding(innerpadding)
+            ) {
+                composable<Screen.Home> {
+                    Text("HOME")
+                }
+                composable<Screen.PrioridadList> {
+                    PrioridadListScreen(
+                        createPrioridad = { navHostController.navigate(Screen.Prioridad(0)) },
+                        goToPrioridadScreen = { navHostController.navigate(Screen.Prioridad(it)) }
+                    )
+                }
+                composable<Screen.Prioridad> {
+                    val prioridadId = it.toRoute<Screen.Prioridad>().prioridadId
+                    PrioridadScreen(
+                        onGoToPrioridadListScreen = { navHostController.navigateUp() },
+                        prioridadId = prioridadId
+                    )
+                }
+            }
         }
-        composable<Screen.Prioridad> {
-            val prioridadId = it.toRoute<Screen.Prioridad>().prioridadId
-            PrioridadScreen(
-                onGoToPrioridadListScreen = { navHostController.navigateUp() },
-                prioridadId = prioridadId
-            )
-        }
+
     }
+
+
 }
