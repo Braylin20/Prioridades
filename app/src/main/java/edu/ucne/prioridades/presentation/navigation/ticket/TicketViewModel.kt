d953973537bbeae86bb46a97dd2a2f181259ff40
Braylin <braylinvasquez20@gmail.com>
2024-09-15 09:41:19 -0400

    Agregando validaciones del ticket

--- app/src/main/java/edu/ucne/prioridades/presentation/navigation/ticket/TicketViewModel.kt
+++ app/src/main/java/edu/ucne/prioridades/presentation/navigation/ticket/TicketViewModel.kt
@@ -4,35 +4,42 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.ucne.prioridades.Data.dao.entities.TicketEntity
+import edu.ucne.prioridades.Data.repository.PrioridadRepository
 import edu.ucne.prioridades.Data.repository.TicketRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 import javax.inject.Inject
 
 
 @HiltViewModel
 class TicketViewModel @Inject constructor(
-    private val ticketRepository: TicketRepository
-) : ViewModel(){
+    private val ticketRepository: TicketRepository,
+) : ViewModel() {
     private val _uiState = MutableStateFlow(UiState())
     val uiState = _uiState.asStateFlow()
 
-    init{
+    init {
         getTickets()
     }
 
-    fun save(){
+    fun save() {
+        if(!isValid()){
+            return
+        }
         viewModelScope.launch {
             ticketRepository.save(_uiState.value.toEntity())
+            nuevo()
         }
     }
 
-    private fun getTickets(){
+    private fun getTickets() {
         viewModelScope.launch {
-            ticketRepository.getTickets().collect{tickets ->
+            ticketRepository.getTickets().collect { tickets ->
                 _uiState.update {
                     it.copy(tickets = tickets)
                 }
@@ -40,15 +47,15 @@ class TicketViewModel @Inject constructor(
         }
     }
 
-    fun delete(ticket: TicketEntity){
+    fun delete(ticket: TicketEntity) {
         viewModelScope.launch {
             ticketRepository.delete(ticket)
         }
     }
 
-    fun selectedTicket(ticketId:Int){
+    fun selectedTicket(ticketId: Int) {
         viewModelScope.launch {
-            if(ticketId >0){
+            if (ticketId > 0) {
                 val ticket = ticketRepository.getTicket(ticketId)
                 _uiState.update {
                     it.copy(
@@ -64,7 +71,22 @@ class TicketViewModel @Inject constructor(
         }
     }
 
-    fun nuevo(){
+    private fun isValid() : Boolean {
+        if (uiState.value.cliente.isBlank() ||
+            uiState.value.asunto.isBlank() ||
+            uiState.value.descripcion.isBlank() ||
+            uiState.value.date.isNullOrBlank() ||
+            uiState.value.prioridadId == null
+        ) {
+            _uiState.update {
+                it.copy(errorMessage = "Todos los campos son requeridos")
+            }
+            return false
+        }
+        return true
+    }
+
+    fun nuevo() {
         _uiState.update {
             it.copy(
                 ticketId = null,
@@ -77,32 +99,38 @@ class TicketViewModel @Inject constructor(
         }
     }
 
-    fun onAsuntoChange(asunto: String){
+    fun onAsuntoChange(asunto: String) {
         _uiState.update {
             it.copy(asunto = asunto)
         }
     }
 
-    fun onClienteChange(cliente: String){
+    fun onClienteChange(cliente: String) {
         _uiState.update {
             it.copy(cliente = cliente)
         }
     }
 
-    fun onDescripcionChange(descripcion: String){
+    fun onDescripcionChange(descripcion: String) {
         _uiState.update {
             it.copy(descripcion = descripcion)
         }
     }
-     fun onDateChange(date: Date){
-         _uiState.update {
-             it.copy(date = date)
-         }
-     }
-    fun onPrioridadIdChange(prioridadId: Int){
+
+    fun onDateChange(date: String) {
+        _uiState.update {
+            it.copy(date = date)
+        }
+    }
+
+    fun onPrioridadIdChange(prioridadId: Int) {
         _uiState.update {
             it.copy(prioridadId = prioridadId)
         }
     }
 
+    fun convertMillisToDate(millis: Long): String {
+        val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
+        return formatter.format(Date(millis))
+    }
 }
